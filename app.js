2121dcce93afc15c7a9172f8a8874809d993f9a8
FireFiya <126648922+FireFiya@users.noreply.github.com>
2025-02-24 13:31:01 +0800

    Create views and update app.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+var path = require("path");
 const express = require('express'); // 引入 Express
 const app = express(); // 建立 Express 應用程式
 
+//設定目錄和引擎
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
 // 設定首頁路由
-app.get('/', (req, res) => {
-    res.send('Working!');
+app.get("/", function(request, response) {
+  response.render("index");
 });
+
+//404
+app.use(function(request, response) {
+    response.status(404).render("404");
+  });
 
 // 啟動伺服器
 app.listen(3000, () => {
